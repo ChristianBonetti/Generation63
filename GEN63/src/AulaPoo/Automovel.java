334cebeb92e05e299b340442c9111ff8de6f3dd0
ChristianBonetti <christian_bonetti@hotmail.com>
2023-04-01 14:51:19 -0300

    exe2

--- GEN63/src/AulaPoo/Automovel.java
+++ GEN63/src/AulaPoo/Automovel.java
@@ -2,9 +2,61 @@ package AulaPoo;
 
 public class Automovel {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String nomeProprietario;
+	private String modelo;
+	private String placa;
+	private int ano;
+	
+	public Automovel(String nomeProprietario, String modelo, String placa, int ano) {
+		super();
+		this.nomeProprietario = nomeProprietario;
+		this.modelo = modelo;
+		this.placa = placa;
+		this.ano = ano;
+	}
+	
+	public Automovel(String modelo,String placa) {
+		this.modelo = modelo;
+		this.placa = placa;
 	}
 
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}	
+	
+	public void imprimirInfo() {
+		System.out.println(nomeProprietario+ " possui um automóvel "+modelo
+				+" com placa "+placa+" e ano: "+ano);
+	}
+	
+	
+	
 }
--- GEN63/src/AulaPoo/TesteAutomovel.java
+++ GEN63/src/AulaPoo/TesteAutomovel.java
@@ -4,10 +4,19 @@ public class TesteAutomovel {
 
 	public static void main(String[] args) {
 		
-		//instanciando um objeto da classe automovel
+		Automovel auto1 = new Automovel("Bruna","Kombi","NOA2008",1998);
 		
-		Automovel auto1 = new Automovel("Bruna","kombi","NOA2008",1998);
-	
+		auto1.imprimirInfo();
+		
+		System.out.println("\nTransferência de Proprietário");
+		
+		auto1.setNomeProprietario("Larissa Pimenta");
+		auto1.imprimirInfo();
+		
+		System.out.println("\nMudança de placa");
+		
+		auto1.setPlaca("ABC8888");
+		auto1.imprimirInfo();
 
 	}
 
